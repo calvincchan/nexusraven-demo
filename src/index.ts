bcd40dfb85b274e0bb951db2bec5f7f51e8209bb
Calvin C Chan <hello@calvin-c.com>
2024-02-12 15:26:35 -0800

    chore: init commit with sample prompt

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { Ollama } from "@langchain/community/llms/ollama";
+import "dotenv/config";
+import { HumanMessage } from "langchain/schema";
+import ora from "ora";
+
+export default async function main() {
+  /** Prepare LLM model with tools */
+  const model = new Ollama({
+    baseUrl: "http://localhost:11434",
+    model: "nexusraven",
+    temperature: 0.01,
+  });
+
+  /** Init message with context. */
+  console.log(`Hello, this is demo of local LLM using Ollama and Nexusraven.`);
+  // Starts the timer
+  console.time("LLM");
+  const spinner = ora("Hello, What's the date of today?").start();
+  const result = await model.invoke([
+    new HumanMessage(`Hello, What's the date of today?`),
+  ]);
+  spinner.stop();
+  console.timeEnd("LLM");
+  console.log("result", result);
+}
+
+(async () => {
+  await main();
+})();
