f409b8f95a4de1e4a7b0a015aa00faebf5163744
Calvin C Chan <hello@calvin-c.com>
2024-02-12 16:04:27 -0800

    chore: applying example prompt in https://github.com/nexusflowai/NexusRaven/blob/main/docs/prompting_readme.md

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,42 @@
 import { Ollama } from "@langchain/community/llms/ollama";
+import { LLM } from "@langchain/core/language_models/llms";
+import { PromptTemplate } from "@langchain/core/prompts";
 import "dotenv/config";
-import { HumanMessage } from "langchain/schema";
 import ora from "ora";
+
+async function generateResponse(model: LLM, question: string) {
+  const prompt = PromptTemplate.fromTemplate(`<human>:
+    OPTION:
+    <func_start>def hello_world(n : int)<func_end>
+    <docstring_start>
+    \"\"\"
+    Prints hello world to the user.
+
+    Args:
+    n (int) : Number of times to print hello world.
+    \"\"\"
+    <docstring_end>
+
+    OPTION:
+    <func_start>def hello_universe(n : int)<func_end>
+    <docstring_start>
+    \"\"\"
+    Prints hello universe to the user.
+
+    Args:
+    n (int) : Number of times to print hello universe.
+    \"\"\"
+    <docstring_end>
+
+    User Query: Question: {question}
+
+    Please pick a function from the above options that best answers the user query and fill in the appropriate arguments.<human_end>
+    `);
+
+  const response = await model.invoke(await prompt.format({ question }));
+
+  return response;
+}
 
 export default async function main() {
   /** Prepare LLM model with tools */
@@ -13,14 +48,15 @@ export default async function main() {
 
   /** Init message with context. */
   console.log(`Hello, this is demo of local LLM using Ollama and Nexusraven.`);
-  // Starts the timer
-  console.time("LLM");
-  const spinner = ora("Hello, What's the date of today?").start();
-  const result = await model.invoke([
-    new HumanMessage(`Hello, What's the date of today?`),
-  ]);
-  spinner.stop();
-  console.timeEnd("LLM");
+
+  const timer = "LLM";
+  const question = "Please print hello universe 31 times.";
+
+  console.time(timer);
+  const spinner = ora(question).start();
+  const result = await generateResponse(model, question);
+  spinner.succeed();
+  console.timeEnd(timer);
   console.log("result", result);
 }
 
